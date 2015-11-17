2595d40c16f6398c43c7b6e369e29f1bf2bc8f8f
Auffret <davy.auffret@gmail.com>
2015-11-17 11:21:56 +0100

    #3 US203 - Client - Liste des pizzas
    Normalisation du code, renommage, par rapport à ce qui est déjà
    présent.... etc

--- products/products.js
+++ products/products.js
@@ -54,15 +54,14 @@ angular.module('pzWebApp.products')
 .controller('pizza_listCtrl', function (userService, pizza_listService) {
 
     var self = this;
-    var id = 1;
     self.title = "Liste de pizzas";
 
-    pizza_listService.promesse.then(function (pizza) {
-        self.pizzas = pizza;
-        
-    }.bind(this))
-    
-    // ...
+    self.pizzas = null; //dessert sélectionné par l'utilisateur
+
+    //liste des desserts
+    pizza_listService.getPizzas().then(function(data){
+       self.desserts = data;
+   })
 
 })
 .filter('inSlicesOf', 
