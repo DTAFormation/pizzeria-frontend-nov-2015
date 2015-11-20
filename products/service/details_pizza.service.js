20067fb15e802f8cf9a6c56daabe4a75357a0782
devinez <tontonsamu@hotmail.fr>
2015-11-20 15:49:00 +0100

    modif details_pizza.service.js #9

--- products/service/details_pizza.service.js
+++ products/service/details_pizza.service.js
@@ -1,16 +1,16 @@
 angular.module('pzWebApp.products').service('detPizService', function ($http, $q) {
 
-        function handleResponse(response) {
+    function handleResponse(response) {
         console.log("Success !")
         return response.data
     }
 
-        this.getPizza = function(id) {
-        	console.log("Get pizzas")        	
-        	var url = 'http://localhost:8080/pizzaUne?id='+id;
-        	console.log(url)
-	        return $http.get(url).
-	        	then(handleResponse)
-    	};
+    this.getPizza = function (id) {
+        console.log("Get pizza")
+        var url = ('http://localhost:8080/pizza/'.id);
+        console.log(url)
+        return $http.get(url).then(handleResponse)
         
+    };
+
 });
