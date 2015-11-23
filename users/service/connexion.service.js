48c4424d2880342464e2436c2b6b07ffc87473ec
Hantar0 <Hantar0@users.noreply.github.com>
2015-11-23 15:45:55 +0100

    #13
    Correction commit

--- users/service/connexion.service.js
+++ users/service/connexion.service.js
@@ -5,6 +5,7 @@ angular.module('pzWebApp.users').service('connexionService', function ($http, pi
 	
 	this.promessePut = function promessePut(login, mdp) {
 		console.log("je passe dans la promesse put : "+login +' mdp : '+mdp)
-		return $http.get(pizzConfig.CLIENT_RESOURCE_URL+'/connexion?login='+login+'&mdp='+mdp)			
+		 var http = $http.get(pizzConfig.CLIENT_RESOURCE_URL+'/connexion?login='+login+'&mdp='+mdp)
+		 return http			
 	}
 });
