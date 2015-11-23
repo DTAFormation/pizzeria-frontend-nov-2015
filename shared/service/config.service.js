e3a4904ee9e6ea32ee5d1c62721980750b062ef1
Auffret <davy.auffret@gmail.com>
2015-11-23 13:58:56 +0100

    #3 minor

--- shared/service/config.service.js
+++ shared/service/config.service.js
@@ -8,14 +8,8 @@ angular.module('pzWebApp.shared').constant('pizzConfig',{
 	'DETAIL_PIZZA_RESOURCE_URL': BACKEND_BASE_URL + '/pizza?id=',
 	'DESSERT_RESOURCE_URL': BACKEND_BASE_URL + '/dessert',
 	'MENU_RESOURCE_URL': BACKEND_BASE_URL + '/menu/',
-<<<<<<< HEAD
 	'PIZZA_RESOURCE_URL': BACKEND_BASE_URL + '/pizza',
 	'IMG_PIZZA_URL' : 'http://dtaformation.github.io/pizzeria-web-image-nov-2015/PIZZA/',
 	'IMG_BOISSON_URL' : 'http://dtaformation.github.io/pizzeria-web-image-nov-2015/BOISSON/',
 	'IMG_DESSERT_URL' : 'http://dtaformation.github.io/pizzeria-web-image-nov-2015/DESSERT/'
-
-=======
-	'PIZZA_RESOURCE_URL': BACKEND_BASE_URL + '/pizza', 
-	'CLIENT_RESOURCE_URL': BACKEND_BASE_URL + '/client'
->>>>>>> 62a4ecf9a020d348db93cdb966c57d4615536361
 })
